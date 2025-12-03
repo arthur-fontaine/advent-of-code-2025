eebed795a21e46d816214aeed403f8298a4defc1
Arthur Fontaine <0arthur.fontaine@gmail.com>
2025-12-04 00:10:27 +0100

    ex3 partB

--- src/03/03.ts
+++ src/03/03.ts
@@ -4,27 +4,31 @@ const banks = readFileSync(`${import.meta.dirname}/banks.txt`, "utf-8").split('\
 
 let t = 0
 for (const bank of banks) {
-  let largest1 = 0
-  let largest1Index = -1
-  for (let i = 0; i < bank.length - 1; i++) {
-    const n = Number(bank[i])
-    if (n > largest1) {
-      largest1 = n
-      largest1Index = i
+  const largests = Array<number>();
+  const largestIndexes = Array<number>();
+
+  const MAX_BATTERY = 12;
+
+  for (let i = 0; i < MAX_BATTERY; i++) {
+
+    let largest = 0;
+    let largestIndex = -1;
+
+    const firstIndex = (largestIndexes.at(-1) ?? -1) + 1;
+    const lastIndex = bank.length - (MAX_BATTERY - i);
+    for (let i = firstIndex; i <= lastIndex; i++) {
+      const n = Number(bank[i])
+      if (n > largest) {
+        largest = n
+        largestIndex = i
+      }
     }
+    largests.push(largest)
+    largestIndexes.push(largestIndex)
+
   }
 
-  let largest2 = 0
-  let largest2Index = -1
-  for (let i = largest1Index + 1; i < bank.length; i++) {
-    const n = Number(bank[i])
-    if (n > largest2) {
-      largest2 = n
-      largest2Index = i
-    }
-  }
-
-  t += Number(`${largest1}${largest2}`)
+  t += Number(largests.join(''))
 }
 
 console.log(t)
